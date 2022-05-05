eac13695eaedf0cddc8550d9acdad7df0e5aa2b0
Ilakyn <49631121+ilkayn01@users.noreply.github.com>
2022-05-05 11:11:37 +0530

    Update nn_pytorch.py

--- neuralnetworks/nn_pytorch.py
+++ neuralnetworks/nn_pytorch.py
@@ -7,10 +7,7 @@ from sklearn.preprocessing import LabelEncoder
 import torch
 import torch.nn as nn
 
-data = pd.read_csv('cancer.csv')
-del data['Unnamed: 0']
-Y = data.iloc[:, -1:]
-X = data.iloc[:,:-1]
+Y, X = data.iloc[:, -1:], data.iloc[:,:-1]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, stratify = Y, random_state=42)
 
@@ -18,11 +15,13 @@ def arr_tensor(x):
   x = torch.tensor(np.array(x), dtype=torch.float)                                #converting train sets to numpy arrays to tensor
   return x
 
-X_train = arr_tensor(X_train)
-Y_train = arr_tensor(Y_train)
+def training(X_train, X_test, Y_train, Y_test, epochs, alpha):
+  X_train = arr_tensor(X_train)
+  Y_train = arr_tensor(Y_train)
 
-td = torch.utils.data.TensorDataset(X_train, Y_train)
+  td = torch.utils.data.TensorDataset(X_train, Y_train)
 
-classifier = nn.Sequential(nn.Linear(in_features=30, out_features=15), nn.ReLU(), nn.Linear(15, 15), nn.ReLU(), nn.Linear(15, 1), nn.Sigmoid())
-criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.006, weight_decay=0.0006)
+  classifier = nn.Sequential(nn.Linear(in_features=30, out_features=15), nn.ReLU(), nn.Linear(15, 15), nn.ReLU(), nn.Linear(15, 1), nn.Sigmoid())
+  criterion = nn.BCELoss()
+  optimizer = torch.optim.Adam(classifier.parameters(), lr=alpha, weight_decay=0.0006)
+  return
